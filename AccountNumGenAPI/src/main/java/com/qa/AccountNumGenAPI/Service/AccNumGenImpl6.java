203c41df780710ad2b965d8040760b7c7a8da220
George Downe <georgedowne06@gmail.com>
2019-03-22 10:30:12 +0000

    refactored

--- AccountNumGenAPI/src/main/java/com/qa/AccountNumGenAPI/Service/AccNumGenImpl6.java
+++ AccountNumGenAPI/src/main/java/com/qa/AccountNumGenAPI/Service/AccNumGenImpl6.java
@@ -6,20 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 public class AccNumGenImpl6 implements AccNumGenService {
-//	@Override
-//	public String generateNumber() {
-//		Random rand = new Random();
-//		int letterChoice = rand.nextInt(3);
-//		char[] letters = ("abc").toCharArray();
-//		int accountNumber = 100000 + rand.nextInt(900000);
-//		String response = Integer.toString(accountNumber);
-//		return letters[letterChoice] + response;
-//	}
-
-	@Autowired
-	AccNumGen numGen;
-
+	@Override
 	public String generateNumber() {
-		return numGen.generateNumber(6);
+		Random rand = new Random();
+		int letterChoice = rand.nextInt(3);
+		char[] letters = ("abc").toCharArray();
+		int accountNumber = 100000 + rand.nextInt(900000);
+		String response = Integer.toString(accountNumber);
+		return letters[letterChoice] + response;
 	}
+
+//	@Autowired
+//	AccNumGen numGen;
+//
+//	public String generateNumber() {
+//		return numGen.generateNumber(6);
+//	}
 }
